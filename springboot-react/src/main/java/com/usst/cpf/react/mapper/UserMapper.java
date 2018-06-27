97a7c6efa1c34dec7a19a30aa46827b447164a71
Pufang Cheng <chengpufang_sx@qiyi.com>
2018-06-27 17:03:21 +0800

    add springboot-react-init

---
+++ springboot-react/src/main/java/com/usst/cpf/react/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ *  Copyright (C) 2018 iQIYI Holdings, Inc.
+ *  All Rights Reserved.
+ *
+ *  This file is part of cloud service. Unauthorized copy of this file, via any
+ *  medium is strictly prohibited. Proprietary and Confidential.
+ *
+ *  Author(s):
+ *      Pufang Cheng <chengpufang_sx@qiyi.com>
+*******************************************************************************/
+
+package com.usst.cpf.react.mapper;
+
+import com.usst.cpf.react.entity.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface UserMapper {
+    @Select("select * from user")
+    List<User> findAllUser();
+}
